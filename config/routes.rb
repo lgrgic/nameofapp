51f195d70f523c1c56b17130e8b682e0fb13a872
lgrgic <lukagrgic14@gmail.com>
2018-04-25 16:40:42 +0200

    3.8 created index, about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'simple_pages/about'
+
+  get 'simple_pages/contact'
+
+  get 'simple_pages/index'
+  get 'simple_pages/about'
+  get 'simple_pages/contact'
+root 'simple_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
