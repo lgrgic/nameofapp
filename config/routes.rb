211045aba01f9cc1d98ef035beba1989a8ee90ef
lgrgic <lukagrgic14@gmail.com>
2018-05-30 17:39:07 +0200

    created orders, landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
-root 'simple_pages#index'
+root 'simple_pages#landing_page'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
