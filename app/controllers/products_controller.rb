faef269c1f63941a71d5308059ffe4684d7741c9
lgrgic <lukagrgic14@gmail.com>
2018-05-31 13:10:58 +0200

    4.4 order models

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    @featured_product = Product.first
   end
 
   # GET /products/1
