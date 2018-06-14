7322bbece4cc34be056602d3894668a07431567a
lgrgic <lukagrgic14@gmail.com>
2018-06-14 20:21:13 +0200

    5.2 forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
   def self.search(search_term)
-		Product.where("name #{like_string} ?", "%#{search_term}%")
+		Product.where("name LIKE ?", "%#{search_term}%")
   end
 end
