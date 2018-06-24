7548ed4a5c6a1a2d163d2f10ac057cb328520fb1
lgrgic <lukagrgic14@gmail.com>
2018-06-24 18:44:44 +0200

    5.4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ‘shielded-wildwood-86691.herokuapp.com’ }
+  config.action_mailer.default_url_options = { host: 'shielded-wildwood-86691.herokuapp.com' }
 
   #For the background image
   config.serve_static_assets = true
