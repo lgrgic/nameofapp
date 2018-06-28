a44ada28857ccdf6d7d595a53ad9b0e58e5a23dc
lgrgic <lukagrgic14@gmail.com>
2018-06-28 18:01:51 +0200

    5.7 comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,16 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
   def self.search(search_term)
   		like_string = Rails.env.production? ? "ilike" : "LIKE"
   		Product.where("name #{like_string} ?", "%#{search_term}%")
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
   end
 end
