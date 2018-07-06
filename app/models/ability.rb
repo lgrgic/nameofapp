c2729ba1e340179c5fc5189c7497ed144c48d5ff
lgrgic <lukagrgic14@gmail.com>
2018-07-06 13:35:38 +0200

    5.10 admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :read, :all .
-    if user.present?
+    if user.admin?
+      can :manage, :all
+    else
       can :manage, Comment, user_id: user.id
       can :manage, User, id: user.id
-      if user.admin?
-        can :manage, :all
   end
 end
