202fbeffd0a6884d7e080dc1a942d51c87c80fe4
lgrgic <lukagrgic14@gmail.com>
2018-07-10 15:59:16 +0200

    sidebar

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,6 +1,12 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "300px";
+    if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("sidebar", "opened");
+    }
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("sidebar", "closed");
+    }
 }
