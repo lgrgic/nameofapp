7638a1971f9950e9a8ab1414e744ea4487c72e97
lgrgic <lukagrgic14@gmail.com>
2018-08-16 15:35:23 +0200

    6.6 fixed payment form and orders

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,31 +2,29 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    token = params[:stripeToken]
     @product = Product.find(params[:product_id])
     @user = current_user
 
-    token = params[:stripeToken]
-    #  this will charge the user's card
+    # This will charge the user's card:
     begin
       charge = Stripe::Charge.create(
-        amount: @product.price * 100,
-        currency: "eur",
-        source: token,
-        description: params[:stripeEmail],
-        receipt_email: params[:stripeEmail]
+        :amount => (@product.price * 100).to_i,
+        :currency => "usd",
+        :source => token,
+        :description => params[:stripeEmail],
+        :receipt_email => params[:stripeEmail]
       )
-
       if charge.paid
-        Order.create(product_id: @product.id, user_id: @user.id, total: @product.price * 100)
-        redirect_to payments_create_path
+        Order.create(product_id: @product.id, user_id: @user.id,
+      total: @product.price.to_i)
       end
 
     rescue Stripe::CardError => e
-      # The card has been declined
+      # When the card has been declined
       body = e.json_body
       err = body[:error]
       flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
-    redirect_to product_path(@product)
-  end
+   end
 end
