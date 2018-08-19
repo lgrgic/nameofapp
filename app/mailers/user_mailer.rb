4bd922bdd38696e59478a0caf12dc7bb1b3253f3
lgrgic <lukagrgic14@gmail.com>
2018-08-19 20:01:44 +0200

    mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,4 @@
 class UserMailer < ApplicationMailer
-  default from: "noreply@planetglobula.com"
 
   def contact_form(email, name, message)
   @message = message
@@ -10,18 +9,15 @@ class UserMailer < ApplicationMailer
 
   def welcome(user)
     @appname = "Planet Globula"
-    mail( from: "welcome@planetglobula.com",
-          to: user.email,
-          subject: "Welcome to #{@appname}!")
+    mail(to: user.email,
+        subject: "Welcome to #{@appname}!")
   end
 
   def order_created(order)
     @appname = "Planet Globula"
     @order = order
     @user = order.user
-    mail(
-    from: "order-confirm@planetglobula.com",
-    to: @user.email,
+    mail(to: @user.email,
     subject: "Your order at #{@appname}")
   end
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -2,4 +2,9 @@ class UserMailerPreview < ActionMailer::Preview
   def contact_form
     UserMailer.contact_form("john@example.com", "John", "Hello World!")
   end
+
+  def welcome
+    UserMailer.welcome(User.first)
+  end
+
 end
