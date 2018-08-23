6667430ec954dd9a1c440210c7cb766e4b4b8725
lgrgic <lukagrgic14@gmail.com>
2018-08-23 15:25:55 +0200

    notifications, feedback email

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-  $(".alert").fadeOut(3500);
+  $(".alert").fadeOut(5000);
 });
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   post 'simple_pages/thank_you'
+  get 'simple_pages/feedback'
   post 'simple_pages/feedback'
   post 'payments/create'
   get 'payments/create'
