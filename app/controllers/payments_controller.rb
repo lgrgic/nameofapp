18dafc61e4638e9c0f6ecc16e4d318399ed25ea6
lgrgic <lukagrgic14@gmail.com>
2018-08-29 16:55:42 +0200

    orders confirmation

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -18,6 +18,7 @@ class PaymentsController < ApplicationController
       if charge.paid
         Order.create(product_id: @product.id, user_id: @user.id,
       total: @product.price.to_i)
+      UserMailer.order_created(@user,@product).deliver_now
       end
 
     rescue Stripe::CardError => e
